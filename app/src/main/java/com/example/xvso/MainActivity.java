3b007ccd0ca94362a032ffacb8db16e3116090d2
dviry75 <dviry75@gmail.com>
2022-09-30 11:26:50 +0200

    נשאר לבדוק ולכתוב מי ניצח

--- app/src/main/java/com/example/xvso/MainActivity.java
+++ app/src/main/java/com/example/xvso/MainActivity.java
@@ -10,40 +10,98 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView tvwin,tvo,tvX;
     boolean x,y;
-    Button b1 ,b2 ,b3,b4,b5,b6,b7,b8,b9,bsO,bsX;
+    Button b[][] = new Button[2][2];
+    Button bsO ,bsX,restart;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 =(Button) findViewById(R.id.bt1);
-        b2 =(Button) findViewById(R.id.bt2);
-        b3 =(Button) findViewById(R.id.bt3);
-        b4 =(Button) findViewById(R.id.bt4);
-        b5 =(Button) findViewById(R.id.bt5);
-        b6 =(Button) findViewById(R.id.bt6);
-        b7 =(Button) findViewById(R.id.bt7);
-        b8 =(Button) findViewById(R.id.bt8);
-        b9 =(Button) findViewById(R.id.bt9);
+        b[0][0] =(Button) findViewById(R.id.bt1);
+        b[0][1] =(Button) findViewById(R.id.bt2);
+        b[0][2] =(Button) findViewById(R.id.bt3);
+        b[1][0] =(Button) findViewById(R.id.bt4);
+        b[1][1] =(Button) findViewById(R.id.bt5);
+        b[1][2] =(Button) findViewById(R.id.bt6);
+        b[2][0] =(Button) findViewById(R.id.bt7);
+        b[2][1] =(Button) findViewById(R.id.bt8);
+        b[2][2] =(Button) findViewById(R.id.bt9);
+        restart = (Button) findViewById(R.id.newS);
         bsO =(Button) findViewById(R.id.stO);
         bsX =(Button) findViewById(R.id.stX);
-        b1.setOnClickListener(this);
-        b2.setOnClickListener(this);
-        b3.setOnClickListener(this);
-        b5.setOnClickListener(this);
-        b4.setOnClickListener(this);
-        b6.setOnClickListener(this);
-        b7.setOnClickListener(this);
-        b8.setOnClickListener(this);
-        b9.setOnClickListener(this);
         bsX.setOnClickListener(this);
         bsO.setOnClickListener(this);
+        restart.setOnClickListener(this);
+        for(int i=0;i<=b.length;i++){
+            for(int j =0;j<=b[i].length;j++){
+                b[i][j].setOnClickListener(this);
+            }
+        }
         tvwin = (TextView) findViewById(R.id.tvwin);
-        tvo = (TextView) findViewById(R.id.tvwin);
-        tvX = (TextView) findViewById(R.id.tvwin);
+        tvo = (TextView) findViewById(R.id.tvwinO);
+        tvX = (TextView) findViewById(R.id.tvwinX);
     }
     public void onClick(View v) {
         if(v == bsX)
         {
+            for(int i=0;i<=b.length;i++){
+                for(int j =0;j<=b[i].length;j++){
+                    if (b[i][j] == v){
+                        int g =0,t=0;
+                        for(int m=0;m<=b.length;m++){
+                            for(int n =0;n<=b[m].length;n++){
+                                if(b[m][n].getText().toString()  == "x"){
+                                    g++;
+                                }
+                                else if(b[m][n].getText().toString()  == "o"){
+                                    t++;
+                                }
+                            }
+                            if(g == t){
+                                b[i][j].setText("x");
+                            }
+                            if(g <= t){
+                                b[i][j].setText("o");
+                            }
+                        }
+                    }
+                }
+            }
 
+        }
+        else  if(v == bsO)
+        {
+            for(int i=0;i<=b.length;i++){
+                for(int j =0;j<=b[i].length;j++){
+                    if (b[i][j] == v){
+                        int g =0,t=0;
+                        for(int m=0;m<=b.length;m++){
+                            for(int n =0;n<=b[m].length;n++){
+                                if(b[m][n].getText().toString()  == "x"){
+                                    g++;
+                                }
+                                else if(b[m][n].getText().toString()  == "o"){
+                                    t++;
+                                }
+                            }
+                            if(g <= t){
+                                b[i][j].setText("x");
+                            }
+                            if(g == t){
+                                b[i][j].setText("o");
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        else if(v == restart){
+            for(int i=0;i<=b.length;i++) {
+                for (int j = 0; j <= b[i].length; j++) {
+                    b[i][j].setText(" ");
+
+                }
+            }
         }
     }
 }
